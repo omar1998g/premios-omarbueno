91a1d6ea00d5ceddaeac99d7e197cff56c1adaf1
Omar González <omar.1998g@gmail.com>
2019-12-22 22:46:18 +0100

    Segundo commit

---
+++ build/precache-manifest.a17832e103a365fcba7956dd0f90acbf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4d9ffa627d39c646e87c4c06d34b329",
+    "url": "/premios-omarbueno/index.html"
+  },
+  {
+    "revision": "ee5cbd63b22afe4211e7",
+    "url": "/premios-omarbueno/static/css/2.64a99b10.chunk.css"
+  },
+  {
+    "revision": "8f2b61126f4a4f40de96",
+    "url": "/premios-omarbueno/static/css/main.fb51042a.chunk.css"
+  },
+  {
+    "revision": "ee5cbd63b22afe4211e7",
+    "url": "/premios-omarbueno/static/js/2.09d59051.chunk.js"
+  },
+  {
+    "revision": "c53da277c84daf0a6ca09a2f259bd576",
+    "url": "/premios-omarbueno/static/js/2.09d59051.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8f2b61126f4a4f40de96",
+    "url": "/premios-omarbueno/static/js/main.ea74e966.chunk.js"
+  },
+  {
+    "revision": "867083de07d83b78f3d1",
+    "url": "/premios-omarbueno/static/js/runtime-main.f4ba717b.js"
+  }
+]);
